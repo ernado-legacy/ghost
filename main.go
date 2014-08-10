21bf746a025a20d3c07ebe751d9c67742d17bdc9
ernado <ernado@ya.ru>
2014-08-10 20:05:53 +0400

    updated mgo

--- main.go
+++ main.go
@@ -1,130 +1,170 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	// "github.com/GeertJohan/go.rice"
-	. "github.com/boltdb/bolt"
-	"github.com/gorilla/websocket"
-	"github.com/julienschmidt/httprouter"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
-	"os"
 	"runtime"
-	"time"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+var (
+	ErrorBadIdent = errors.New("Unable to parse id")
+)
+
+type Post struct {
+	ID   bson.ObjectId `json:"id" bson:"_id"`
+	Text string        `json:"text" bson:"text"`
+}
+
+type DataBase struct {
+	session *mgo.Session
+	db      *mgo.Database
+	posts   *mgo.Collection
+}
+
+func ParseID(s string) (id bson.ObjectId, err error) {
+	if !bson.IsObjectIdHex(s) {
+		return id, ErrorBadIdent
+	}
+	return bson.ObjectIdHex(s), nil
+}
+
+func (db *DataBase) AddPost(post Post) error {
+	return db.posts.Insert(post)
+}
+
+func (d *DataBase) GetAllPosts() (posts []Post, err error) {
+	return posts, d.posts.Find(nil).All(&posts)
+}
+
+func (d *DataBase) GetPost(id bson.ObjectId) (*Post, error) {
+	query := bson.M{"_id": id}
+	post := &Post{}
+	return post, d.posts.Find(query).One(post)
 }
 
 const (
-	MAX_LATENCY_MS = 2
+	postsCollection = "posts"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
-func GetId(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "001")
-}
-
-func Realtime(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	pongs := make(chan time.Time)
-
-	conn.SetPongHandler(func(s string) error {
-		log.Println("[realtime]", "pong")
-		go func() {
-			defer func() {
-				e := recover()
-				if e != nil {
-					log.Println("[realtime]", "timed out pong")
-				}
-			}()
-			pongs <- time.Now()
-		}()
-		return nil
-	})
-
+func GetDB(name, url string) (*DataBase, error) {
+	db := DataBase{}
+	session, err := mgo.Dial(url)
 	if err != nil {
-		log.Println("upgrade error", err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return nil, err
+	}
+
+	db.session = session
+	db.db = session.DB(name)
+	db.posts = db.db.C(postsCollection)
+
+	return &db, nil
+}
+
+type Application struct {
+	DB  *DataBase
+	ids chan int64
+}
+
+func ShowErr(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+func (a *Application) StartIDDispatcher() {
+	a.ids = make(chan int64, 100)
+	var id int64
+	log.Println("dispatcher started")
+	for {
+		id += 1
+		a.ids <- id
+	}
+}
+
+func (a *Application) GetID() string {
+	log.Println("dispatching")
+	id := <-a.ids
+	return fmt.Sprintf("%x", id)
+}
+
+func (a *Application) GetPost(c *gin.Context) {
+	idStr := c.Params.ByName("id")
+	id, err := ParseID(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, ShowErr(err))
 		return
 	}
-	log.Println("[realtime]", "connected")
-	defer func() {
-		conn.Close()
-		log.Println("[realtime] closed")
-	}()
-
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("[realtime]", "read message error", err)
-			return
-		}
-		if err = conn.WriteMessage(messageType, p); err != nil {
-			log.Println("[realtime]", "write message error", err)
-			return
-		}
-		deadline := time.Now().Add(time.Millisecond * MAX_LATENCY_MS)
-		if err = conn.WriteControl(websocket.PingMessage, nil, deadline); err != nil {
-			log.Println("[realtime]", "ping error", err)
-			return
-		}
-		go func() {
-			now := time.Now()
-			select {
-			case <-time.After(time.Millisecond * MAX_LATENCY_MS):
-				log.Println("[realtime]", "timeout")
-				close(pongs)
-				conn.Close()
-				return
-			case t := <-pongs:
-				delta := t.Sub(now)
-				log.Println("[realtime]", "pinged", delta)
-				conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprint("PING ", delta)))
-			}
-		}()
+	post, err := a.DB.GetPost(id)
+	if err == mgo.ErrNotFound {
+		c.JSON(http.StatusNotFound, ShowErr(err))
+		return
 	}
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, ShowErr(err))
+		return
+	}
+	a.Render(c, post)
+}
+
+func (a *Application) GetAllPosts(c *gin.Context) {
+	posts, err := a.DB.GetAllPosts()
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusInternalServerError, ShowErr(err))
+		return
+	}
+	a.Render(c, posts)
+}
+
+func (a *Application) Render(c *gin.Context, val interface{}) {
+	c.Writer.WriteHeader(200)
+	c.Writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(val); err != nil {
+		c.ErrorTyped(err, gin.ErrorTypeInternal, val)
+		c.Abort(http.StatusInternalServerError)
+	}
+}
+
+func (a *Application) AddPost(c *gin.Context) {
+	var post Post
+	decoder := json.NewDecoder(c.Req.Body)
+	if err := decoder.Decode(&post); err != nil {
+		c.JSON(http.StatusBadRequest, ShowErr(err))
+		return
+	}
+	post.ID = bson.NewObjectId()
+	if err := a.DB.AddPost(post); err != nil {
+		c.JSON(http.StatusInternalServerError, ShowErr(err))
+		return
+	}
+	a.Render(c, "ok")
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-	router.GET("/realtime", Realtime)
-	// router.GET("/peerjs/:id/:token/id", GetId)
-	// router.GET("/peerjs/id", GetId)
-
-	// static files
-	// router.Handler("/static", path, http.FileServer(rice.MustFindBox("static").HTTPBox()))
-	router.ServeFiles("/static/*filepath", http.Dir("static"))
-	db, _ := Open("test", 0666)
-	defer os.Remove(db.Path())
-	defer db.Close()
-
-	// Start a write transaction.
-	db.Update(func(tx *Tx) error {
-		// Create a bucket.
-		tx.CreateBucket([]byte("widgets"))
-
-		// Set the value "bar" for the key "foo".
-		tx.Bucket([]byte("widgets")).Put([]byte("foo"), []byte("bar"))
-		return nil
+	db, err := GetDB("gost-dev", "localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	app := &Application{}
+	app.DB = db
+	go app.StartIDDispatcher()
+	id := app.GetID()
+	log.Println(id)
+	r := gin.Default()
+	r.GET("/post", app.GetAllPosts)
+	r.PUT("/post", app.AddPost)
+	r.GET("/post/:id/", app.GetPost)
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
 	})
-
-	// Read value back in a different read-only transaction.
-	db.View(func(tx *Tx) error {
-		value := tx.Bucket([]byte("widgets")).Get([]byte("foo"))
-		fmt.Printf("The value of 'foo' is: %s\n", value)
-		return nil
-	})
-	log.Fatal(http.ListenAndServe(":8081", router))
+	r.Static("/static", "static")
+	// Listen and server on 0.0.0.0:8080
+	r.Run(":8080")
 }
